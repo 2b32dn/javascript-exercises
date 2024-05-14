3aabf8944db1fc978adb0a5ac1886b97b1c4fa5a
Dickson Lee <dicksonT_lee@hotmail.com>
2024-05-14 15:20:25 -0400

    push all but 12

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  if (arr.length == 0) return 0;
+  return arr.reduce((accumulator, currentValue) => accumulator + currentValue);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((accumulator, currentValue) => accumulator * currentValue);
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num == 0 || num == 1) return 1;
+  return num * factorial(num - 1);
 };
 
 // Do not edit below this line
@@ -29,5 +31,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let string = str.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~() ]/g, "");
+  let palinStr = string.split("").reverse().join("");
 
+  if (palinStr == string) return true;
+  return false;
 };
 
 // Do not edit below this line
